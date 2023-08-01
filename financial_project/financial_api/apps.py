2644b954f71b41892db1551d89c429d9a9654fcf
David Zumaquero Mairena <zumaquerodavid@gmail.com>
2023-08-01 17:59:57 +0200

    fix: remove typo in financial

--- financial_project/financial_api/apps.py
+++ financial_project/financial_api/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FinantialApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'finantial_api'
+    name = 'financial_api'
--- financial_project/financial_project/urls.py
+++ financial_project/financial_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for finantial_project project.
+URL configuration for financial_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
